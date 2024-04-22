beb18ca3c5deeb49239b8616bcbe138b62916786
kristian0808 <kristianlleshaj@hotmail.com>
2024-04-22 14:03:29 +0200

    complex types

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Returns the square value of the int or float"""
+
+from typing import Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> float:
+    """Returns the square value of the int or float"""
+    return (k, v * v)

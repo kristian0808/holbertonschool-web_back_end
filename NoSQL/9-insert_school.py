214b27a17d3bf072a3ffda0ddf163730b90c0379
kristian0808 <kristianlleshaj@hotmail.com>
2024-05-02 10:34:35 +0200

    insert

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Inserts a new document in a collection based on kwargs.
+
+    Args:
+      mongo_collection: pymongo collection object.
+      **kwargs: Keyword arguments representing the document fields and values.
+
+    Returns:
+      The _id of the newly inserted document.
+    """
+    # Insert the document with the provided kwargs
+    result = mongo_collection.insert_one(kwargs)
+
+    # Return the _id of the newly inserted document
+    return result.inserted_id

d581dd722640acf81322b81b4f5ff51b3495c176
kristian0808 <kristianlleshaj@hotmail.com>
2024-05-09 15:21:05 +0200

    block scope

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+/* eslint-disable*/
+export default function taskBlock(trueOrFalse) {
+    let task = false;
+    let task2 = true;
+    if (trueOrFalse) {
+        task = true;
+        task2 = false;
+    }
+    return [task, task2];
+}

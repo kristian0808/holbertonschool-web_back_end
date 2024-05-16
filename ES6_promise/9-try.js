b3e67b8e0c61998ee171c9f335991cbc03dca4ec
kristian0808 <kristianlleshaj@hotmail.com>
2024-05-17 00:22:27 +0200

    error catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,13 @@
+/*eslint-disable*/
+
+export default function guardrail(mathFunction) {
+    const queue = [];
+    try {
+        const result = mathFunction();
+        queue.push(result);
+    } catch (error) {
+        queue.push(`Error: ${error.message}`);
+    }
+    queue.push('Guardrail was processed');
+    return queue;
+}
